e13e6502034cc0a9cad6afb72697414d0e498af3
Les Kinney <les.kinney@aa.com>
2018-11-02 19:04:28 -0700

    checking length

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -36,7 +36,7 @@ class Fib extends Component {
 
   renderSeenIndexes() {
     let result = '';
-    if(this.state.seenIndexes)
+    if(this.state.seenIndexes.length > 0)
     {
       result = this.state.seenIndexes.map(i => i).join(', ');    
     }
